1b337652438624aa3fd901819487ca344c5c0b93
sashabrayb <sashabraybrooke4866@gmail.com>
2026-07-08 20:53:13 +1000

    Created basic homepage to navigate to

---
+++ fitness_tracker/lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/images/background.png',
+            fit: BoxFit.cover,
+          ),
+          // add your home screen content here
+        ],
+      ),
+    );
+  }
+}
